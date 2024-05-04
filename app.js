14c6ff534dac39d5bd018833a2ef656c9bfef714
Rasel Ahmed Bijoy <153086738+raselwebdevv@users.noreply.github.com>
2024-05-04 08:31:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const imgTag = document.querySelector(".image");
-const allImage = ["./images/1.png", "./images/2.png", "./images/3.png", "./images/4.png", "./images/5.png"];
+const allImage = ["1.png", "2.png", "3.png", "4.png", "5.png"];
 const imgN = document.querySelector(".imgN");
 const totalImgN = document.querySelector(".totalImgN");
 
